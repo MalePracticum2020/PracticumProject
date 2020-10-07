90915a1c75ce5b7069bc1377cb23be6ed8896d65
sebnunez6 <snunez6@miners.utep.edu>
2020-10-07 13:11:17 -0400

    Adding installer no one touch it

--- installer.sh
+++ installer.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-PACKAGEPROGRAMS= "python3-pip python3-venv git python3-pyqt5.qtwebengine"
+PACKAGEPROGRAMS="python3-pip python3-venv git python3-pyqt5.qtwebengine"
 
 if [ -x "/usr/bin/apt-get" ]; then
-    OS_VERSION="Debian"
-    apt-get -y install $PACKAGEPROGRAMS
+    sudo apt-get update
+    sudo apt-get -y install $PACKAGEPROGRAMS
 elif [ -x "/usr/bin/yum" ]; then
-    OS_VERSION="CentOS"
-    yum install -y $PACKAGEPROGRAMS
+    sudo yum install -y $PACKAGEPROGRAMS
 else
     echo "$OUTPUT_ERROR_PREFIX Distribution not supported"
     exit 1
@@ -15,4 +14,4 @@ fi
 
 pip install pip --upgrade
 
-pip install "PyQT5 plotly dash Flask pandas plotly-express cachetools"
+pip install PyQT5 plotly dash Flask pandas plotly-express cachetools
